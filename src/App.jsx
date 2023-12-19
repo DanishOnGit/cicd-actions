2f321ea7fdd62cc46a462497346cd907a58f527b
DanishOnGit <youcandoit1911@gmail.com>
2023-12-19 10:35:33 +0000

    update aml

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,27 @@ function App() {
 			<p className="read-the-docs">Hello world</p>
 			<p className="read-the-docs">How are you?</p>
 			<p className="read-the-docs">I am fine?</p>
+			{/* build-project:
+   runs-on: ubuntu-latest
+   env:
+       VERCEL_PROJECT_ID: ${{ secrets.VERCEL_PROJECT_ID }}
+       VERCEL_ORG_ID: ${{ secrets.VERCEL_ORG_ID }}
+       VERCEL_TOKEN: ${{ secrets.VERCEL_TOKEN}}
+   steps:
+    - name: Checkout code
+      uses: actions/checkout@v2
+    - name: Install nodejs
+      uses: actions/setup-node@v2
+      with:
+       node-version: "17.0.0"
+    - name: Install deps
+      run: yarn
+    - name: Build the project
+      run: yarn build
+    - name: Deploy the project
+      run: yarn run deploy
+    - name: Completion
+      run: echo "Done!" */}
 		</>
 	)
 }
